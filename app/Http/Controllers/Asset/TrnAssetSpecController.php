7a661a96766129d876b90c3d0ba28b66ed768b8d
dvaled <davaalif0@gmail.com>
2024-12-10 16:09:56 +0700

    change redirect and api url

--- app/Http/Controllers/Asset/TrnAssetSpecController.php
+++ app/Http/Controllers/Asset/TrnAssetSpecController.php
@@ -161,10 +161,10 @@ class  TRNAssetSpecController extends Controller
                 return redirect()->route('transaction.asset.laptop', ['assetcode' => $assetcode])
                                  ->with('success', 'Asset created successfully!');
             } else if ($category == 'MOBILE') {
-                return redirect()->route('detailAsset.mobile', ['assetcode' => $assetcode])
+                return redirect()->route('transaction.asset.mobile', ['assetcode' => $assetcode])
                                  ->with('success', 'Asset created successfully!');
             } else {
-                return redirect()->route('detailAsset.others', ['assetcode' => $assetcode])
+                return redirect()->route('transaction.asset.others', ['assetcode' => $assetcode])
                                 ->with('success', 'Asset created successfully!');}
             
         } catch (\GuzzleHttp\Exception\RequestException $e) {
@@ -294,7 +294,7 @@ class  TRNAssetSpecController extends Controller
                 return redirect()->route('detailAsset.mobile', ['assetcode' => $assetcode])
                                  ->with('success', 'Asset created successfully!');
             } else {
-                return redirect()->route('detailAsset.others', ['assetcode' => $assetcode])
+                return redirect()->route('transaction.asset.laptop', ['assetcode' => $assetcode])
                                 ->with('success', 'Asset created successfully!');}
         
             return redirect()->back()->with("success", "Data has been updated successfully");
