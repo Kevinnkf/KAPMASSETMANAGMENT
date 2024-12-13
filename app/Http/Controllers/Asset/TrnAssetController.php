cfb47dce601ebf7f103039e26afddcbf8c45e1f2
Kevin <khalfanifadillah.com>
2024-12-13 15:23:35 +0700

    Change name file for export excel data

--- app/Http/Controllers/Asset/TrnAssetController.php
+++ app/Http/Controllers/Asset/TrnAssetController.php
@@ -769,7 +769,7 @@ class TrnAssetController extends Controller
             "data" => session('userdata')
         ];
 
-        $excel = Excel::download(new AssetExport($data), "testStyle.xlsx");
+        $excel = Excel::download(new AssetExport($data), "Data Aset KAPM.xlsx");
         return $excel;
 
     
